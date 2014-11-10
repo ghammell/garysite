c5d5a90c1d74deec2ad16549832768512c941b23
Gary Hammell <ghammell1@gmail.com>
2014-11-10 15:07:44 -0800

    adjust moon background, add descriptions to posts

--- app/assets/javascripts/background.js
+++ app/assets/javascripts/background.js
@@ -7,15 +7,16 @@ var bindScrollOnMoon = function(object, horizontalDirection, verticalDirection) 
   var lastScrollTop = 0;
   $(window).scroll(function(event){
     var currentTop = $(this).scrollTop();
-    position = $(object).css(horizontalDirection).replace('px','')
-    horizontalMovement = 3
+    horizontalPosition = $(object).css(horizontalDirection).replace('px','')
+    verticalPosition = $(object).css(verticalDirection).replace('px','')
+    horizontalMovement = 2
     verticalMovement = 1
     if (currentTop > lastScrollTop){
-      $(object).css(horizontalDirection, parseInt(position) + horizontalMovement + 'px')
-      $(object).css(verticalDirection, parseInt(position) + verticalMovement + 'px')
+      $(object).css(horizontalDirection, parseInt(horizontalPosition) + horizontalMovement + 'px')
+      // $(object).css(verticalDirection, parseInt(verticalPosition) + verticalMovement + 'px')
     } else {
-      $(object).css(horizontalDirection, parseInt(position) - horizontalMovement + 'px')
-      $(object).css(verticalDirection, parseInt(position) - verticalMovement + 'px')
+      $(object).css(horizontalDirection, parseInt(horizontalPosition) - horizontalMovement + 'px')
+      // $(object).css(verticalDirection, parseInt(verticalPosition) - verticalMovement + 'px')
     }
     lastScrollTop = currentTop;
   });
