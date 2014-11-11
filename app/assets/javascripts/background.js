199084ce47a245fdf573b8a929efa8260f81457f
Gary Hammell <ghammell1@gmail.com>
2014-11-10 20:38:57 -0800

    transition banner on scroll, add images for external links

--- app/assets/javascripts/background.js
+++ app/assets/javascripts/background.js
@@ -2,24 +2,20 @@ $(document).ready(function(){
   window.onbeforeunload = function(){
     window.scrollTo(0,0);
   }
-  bindScrollOnMoon('#moon_image', 'right', 'bottom')
+  bindScrollOnMoon('#moon_image', 'right')
 })
 
 
-var bindScrollOnMoon = function(object, horizontalDirection, verticalDirection) {
+var bindScrollOnMoon = function(object, horizontalDirection) {
   var lastScrollTop = 0;
   $(window).scroll(function(event){
     var currentTop = $(this).scrollTop();
     horizontalPosition = $(object).css(horizontalDirection).replace('px','')
-    // verticalPosition = $(object).css(verticalDirection).replace('px','')
     horizontalMovement = 1
-    // verticalMovement = 1
     if (currentTop > lastScrollTop){
       $(object).css(horizontalDirection, parseInt(horizontalPosition) + horizontalMovement + 'px')
-      // $(object).css(verticalDirection, parseInt(verticalPosition) + verticalMovement + 'px')
     } else {
       $(object).css(horizontalDirection, parseInt(horizontalPosition) - horizontalMovement + 'px')
-      // $(object).css(verticalDirection, parseInt(verticalPosition) - verticalMovement + 'px')
     }
     lastScrollTop = currentTop;
   });
--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   bindPostHoverEvents()
+  bindBannerShowEvent()
 })
 
 var bindPostHoverEvents = function() {
@@ -13,4 +14,16 @@ var bindPostHoverEvents = function() {
       post.children('.more').fadeIn()
     })
   })
+}
+
+var bindBannerShowEvent = function() {
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+    var currentTop = $(this).scrollTop();
+    if (currentTop > 200) {
+      $('#personal_info_banner').css('top', '0px')
+    } else {
+      $('#personal_info_banner').css('top', '-82px')
+    }
+  });
 }
